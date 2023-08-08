4f2f556bdf81ef85eed39bd30f425adca114d41f
SUZUKI <38928546+suzuki84g@users.noreply.github.com>
2023-08-08 23:43:00 +0900

    記載整理

--- .zshrc
+++ .zshrc
@@ -3,5 +3,8 @@ SCRIPT_DIR=$HOME/dotfiles
 source $SCRIPT_DIR/zsh/alias.zsh
 source $SCRIPT_DIR/zsh/path.zsh
 
+# homebrew
+# `.zprofile` に記述する為、管理外。
+
 # anyenv
 eval "$(anyenv init -)"
--- zsh/path.zsh
+++ zsh/path.zsh
@@ -1,8 +1,1 @@
 ### PATH ###
-# anyenv
-export PATH="$HOME/.anyenv/bin:$PATH"
-eval "$(anyenv init -)"
-# homebrew Python
-export PATH=/usr/local/opt/python@3.10/libexec/bin:$PATH
-# brew
-export PATH=/usr/local/sbin:$PATH
